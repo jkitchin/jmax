6083ce9cf0e04d4401f45a188916fce7c171b23e
John Kitchin <jkitchin@cmu.edu>
2013-07-03 08:47:28 -0400

    deleted

--- emacs-24.2/lisp/term/vt102.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t -*-
-
-(defun terminal-init-vt102 ()
-  "Terminal initialization function for vt102."
-  (tty-run-terminal-initialization (selected-frame) "vt100"))
-
-;;; vt102.el ends here
--- emacs-24.2/lisp/term/vt125.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t -*-
-
-(defun terminal-init-vt125 ()
-  "Terminal initialization function for vt125."
-  (tty-run-terminal-initialization (selected-frame) "vt100"))
-
-;;; vt125.el ends here
