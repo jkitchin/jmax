cda657c05918734dfa3c9c50222b482730807f46
John Kitchin <jkitchin@andrew.cmu.edu>
2014-03-26 17:58:54 -0400

    added libxml2 from http://sourceforge.net/projects/ezwinports/

---
+++ emacs-24.3/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/d/usr/lib"
+XML2_LIBS="-lxml2 -lz   -liconv  "
+XML2_INCLUDEDIR="-I/d/usr/include/libxml2"
+MODULE_VERSION="xml2-2.7.8"
+
