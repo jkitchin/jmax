ad649cf96f5d3dcec45e40357ad02eb0d7d45d61
John Kitchin <jkitchin@andrew.cmu.edu>
2014-04-25 19:46:42 -0400

    deleted some stuff we do not need

--- kitchingroup-mode.el
+++ kitchingroup-mode.el
@@ -1,10 +1,26 @@
-;;; kitchingroup-mode --- Summary
-;;;
-;;;
-;;; Commentary:
-;;; some utilities for the kitchingroup
-;;;
+;;; kitchingroup-mode.el --- Summary
+;; Copyright(C) 2014 John Kitchin
 
+;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
+;; This file is not currently part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+;; some utilities for the kitchingroup
 
 (require 'easymenu)
 
@@ -14,17 +30,6 @@
     (define-key map (kbd "C-c e") 'email-region)
     map)
   "Keymap for kitchingroup mode.")
-
-
-(defun find-non-ascii-characters ()
-  (interactive)
-  (occur "[^[:ascii:]]"))
-
-(defun kg-open-my-org ()
-  "Open my org-file
-
-you have to define `my-kitchingroup-org-file' in your init files"
-  (find-file my-kitchingroup-org-file))
 
 (easy-menu-define my-menu kitchingroup-mode-map "My own menu"
   '("KitchinGroup"
