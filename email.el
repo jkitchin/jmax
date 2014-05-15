bbc37fc37297096fc3b020512448f772e0175777
John Kitchin <jkitchin@andrew.cmu.edu>
2014-05-15 09:50:30 -0400

    removed add citation command, it was causing an error I did not want to fix now

--- email.el
+++ email.el
@@ -11,7 +11,7 @@
     (setq content-with-citations (with-temp-buffer
 				   (org-mode)
 				   (insert content)
-				   (org-ref-extract-bibtex-entries)
+				   ;(org-ref-extract-bibtex-entries)
 				   (buffer-string)))
     (compose-mail-other-frame)
     (message-goto-body)
@@ -28,7 +28,7 @@
       (set-buffer (get-buffer-create "*org-email-region*"))
       (org-mode)
       (insert content)
-      (org-ref-extract-bibtex-entries)
+      ;(org-ref-extract-bibtex-entries)
       (compose-mail-other-frame)
       (mml-attach-buffer "*org-email-region*")
       (message-goto-to))))
