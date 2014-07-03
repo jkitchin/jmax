e2cac5145d68c93bd1448703051b4b60e0b7467f
John Kitchin <jkitchin@andrew.cmu.edu>
2014-07-03 12:54:08 -0400

    improvements

--- techela.el
+++ techela.el
@@ -10,46 +10,68 @@
 
 ;;; Code:
 
-(defvar tq-git-server "techela.cheme.cmu.edu" "The git server")
-(defvar tq-root-directory nil "location to clone the course and student work to")
-(defvar tq-course-directory nil "directory wher the course content is. It is inside `tq-root-directory'")
+(defvar tq-git-server "techela.cheme.cmu.edu"
+  "The git server where techela courses are served.")
+
+(defvar tq-root-directory nil
+  "Location to clone the course and student work to. This location is not a git repository.")
+
+(defvar tq-course-directory nil
+  "Directory where the course content is. This will be a git repository. It is inside `tq-root-directory'")
+
 (defvar tq-current-course nil "Store value of current course")
 
-(defun tq-syllabus (tq-course-name)
-  "Open the syllabus for TQ-COURSE-NAME.
+;; options for git
+(defvar GIT_SSH (format "GIT_SSH=%s" (expand-file-name "techela_ssh" starter-kit-dir)))
 
-First we look up the course in `tq-courses'. If it is not there,
-we run some setup code to clone the course and save some
-variables.
+(defvar tq-debug nil "Whether to debug or not. non-nil triggers some debug action")
+
+(defun tq-log (format-string &rest args)
+  "log a message to *techela-log*. Same syntax as `message'.
+The first argument is a format control string, and the rest are data
+to be formatted under control of the string.  See `format' for details.
+
+Note: Use (tq-log \"%s\" VALUE) to print the value of expressions and
+variables to avoid accidentally interpreting `%' as format specifiers."
+  (with-current-buffer (get-buffer-create "*techela-log*")
+    (end-of-buffer)
+    (insert "\n")
+    (insert (apply 'format format-string args))))
 
 
-The course should exist at tq-course-name@techela.cheme.cmu.edu:course
+(defmacro with-current-directory (directory &rest body)
+  "Runs BODY with the working directory temporarily set to
+DIRECTORY. DIRECTORY is expanded"
+  `(let ((default-directory ,(file-name-as-directory (expand-file-name (eval directory)))))
+     ,@body))
 
-The user id_rsa.pub key must be registered in the course.
+
+(defun mygit (git-command)
+  "Run GIT-COMMAND in custom environment.
+
+For example:
+ (mygit \"git clone org-course@techela.cheme.cmu.edu:course\")
+
+You should make
+Sets GIT_SSH to `GIT_SSH', and temporarily modifies the process environment before running git. `GIT_SSH' points to a shell script that runs ssh in batch mode.
+
+returns (status output)
 "
-  (interactive
-   (list
-    (ido-completing-read
-     "Course name: "
-     (if (boundp 'tq-courses)
-	 (mapcar (lambda (x) (car x)) tq-courses)
-       '()))))
+  (interactive "sgit command: ")
+  (let ((process-environment (cons GIT_SSH process-environment))
+        (status) (output))
+    (when (get-buffer "*mygit-process*") (kill-buffer "*mygit-process*"))
+    (tq-log "\nRunning \"%s\"\n  CWD = %s" git-command default-directory)
+    (setq status (call-process-shell-command git-command nil "*mygit-process*"))
+    (setq output (with-current-buffer "*mygit-process*" (buffer-string)))
+    (tq-log "  status = %s" status)
+    (tq-log "  output = %s" output)
+    (list status output)))
 
-  ;; create this variable if it does not exist
-  ;; it is defined in custom.el
-  (unless (boundp 'tq-courses)
-    (customize-save-variable 'tq-courses '()))
+(defun ta-setup-user ()
+  "Makes sure these variables are set:
 
-  ;; Set this for the current session
-  (setq tq-current-course tq-course-name)
-
-  ;; define directories to variables if they exist
-  (when (and (boundp 'tq-courses) tq-courses)
-    (let ((vars (cdr (assoc tq-course-name tq-courses))))
-      (when vars
-	(setq tq-root-directory (nth 0 vars)
-	      tq-course-directory (nth 1 vars)))))
-
+`*andrewid*', `user-full-name'"
   ;; make sure basic variables are defined
   ;; ANDREWID
   (unless (and (boundp '*andrewid*) *andrewid*)
@@ -59,35 +81,23 @@ The user id_rsa.pub key must be registered in the course.
   ;; Full name
   (unless (and (boundp 'user-full-name) user-full-name)
     (customize-save-variable 'user-full-name
-			     (read-from-minibuffer "Enter your full name: ")))
+			     (read-from-minibuffer "Enter your full name: "))))
+
+
+(defun ta-setup-email ()
+  "Saves email setup if needed.
+
+Makes sure these variables are set.
+`user-mail-address', `send-mail-function',
+`message-send-mail-function', `smtpmail-smtp-server',
+`smtpmail-starttls-credentials', `smtpmail-smtp-service'"
 
   ;; email address
   (unless (and (boundp 'user-mail-address) user-mail-address)
     (customize-save-variable
      'user-mail-address
-     (read-from-minibuffer "Enter your full name: ")))
-
-  ;; This is where we will clone the course, and later student work
-  (unless (and (boundp 'tq-root-directory) tq-root-directory)
-    (customize-save-variable
-     'tq-root-directory
-     (file-name-as-directory
-      (ido-read-directory-name "Enter directory to download course: " nil
-			       (format "~/Desktop/%s" tq-course-name)))))
-
-  ;; make directory if needed
-  (unless (file-exists-p tq-root-directory)
-    (make-directory tq-root-directory))
+     (read-from-minibuffer "Enter your email address: ")))
   
-  ;; clone course if we need it. This will be in a repo called "course"
-  (unless (and tq-course-directory (file-exists-p tq-course-directory))
-    (let ((default-directory (file-name-as-directory tq-root-directory)))
-      (shell-command (format "git clone %s@%s:course" tq-course-name tq-git-server))))
-
-  ;; we use this later
-  (setq tq-course-directory (file-name-as-directory
-			     (expand-file-name "course" tq-root-directory)))
-     
   (unless (and (boundp 'send-mail-function) send-mail-function)
     (customize-save-variable 'send-mail-function 'smtpmail-send-it))
 
@@ -103,71 +113,133 @@ The user id_rsa.pub key must be registered in the course.
      `((,smtpmail-smtp-server 587 nil nil))))
 
   (unless (and (boundp 'smtpmail-smtp-service) smtpmail-smtp-service)
-    (customize-save-variable 'smtpmail-smtp-service 587))
+    (customize-save-variable 'smtpmail-smtp-service 587)))
 
-  (if tq-courses
-      (add-to-list 'tq-courses `(,tq-course-name . (,tq-root-directory ,tq-course-directory)))
-    (setq tq-courses `((,tq-course-name . (,tq-root-directory ,tq-course-directory)))))
-  (customize-save-variable 'tq-courses tq-courses)
+
+(defun techela (tq-course-name)
+  "Open the syllabus for TQ-COURSE-NAME.
+
+First we look up the course in `techela-courses'. If it is not there,
+we run some setup code to clone the course and save some
+variables.
+
+The course should exist at tq-course-name@techela.cheme.cmu.edu:course
+
+The user id_rsa.pub key must be registered in the course.
+"
+  (interactive
+   (list
+    (ido-completing-read
+     "Course name: "
+     (if (boundp 'techela-courses)
+	 (mapcar (lambda (x) (car x)) techela-courses)
+       '()))))
+
+  (ta-setup-user)
+  (ta-setup-email)
+
+  ;; Set this for the current session
+  (setq tq-current-course tq-course-name)
+
+  ;; initialize to nil
+  (setq tq-root-directory nil
+	tq-course-directory nil)
+
+  ;; load directories to variables if they exist
+  (when (and (boundp 'techela-courses) techela-courses)
+    (let ((vars (cdr (assoc tq-course-name techela-courses))))
+      (if vars 
+	  (progn  ;; we have some data
+	    (setq tq-root-directory (nth 0 vars)
+		  tq-course-directory (nth 1 vars)))
+	;; no course found, set these to nil so we have to start over
+	(setq tq-root-directory nil
+	      tq-course-directory nil))))
+  
+  ;; This is where we will clone the course, and later student work If
+  ;; this variable does not exist, it was not saved in custom.el we
+  ;; prompt for where to put the course.  if tq-root-directory is
+  ;; defined here, it means it was read above, and we skip this step.
+  (unless (and (boundp 'tq-root-directory) tq-root-directory)
+    (setq tq-root-directory
+	  (file-name-as-directory
+	   (ido-read-directory-name "Enter directory to download course: " nil
+				    (format "~/Desktop/%s" tq-course-name)))
+	  tq-course-directory (file-name-as-directory
+			       (expand-file-name "course" tq-root-directory))))
+  
+  ;; make root directory if needed, including parents
+  (unless (file-exists-p tq-root-directory)
+    (make-directory tq-root-directory t))
+
+  
+  ;; clone course if we need it. This will be in a repo called "course"
+  ;; do not clone if the directory exists.
+  (unless (and tq-course-directory (file-exists-p tq-course-directory))
+    (let ((default-directory (file-name-as-directory tq-root-directory)))
+      (mygit (format "git clone %s@%s:course" tq-course-name tq-git-server))))
+
+  ;; Now, we add the defined course to
+  (if (and (boundp 'techela-courses) techela-courses)
+      (add-to-list 'techela-courses (cons tq-course-name (list tq-root-directory tq-course-directory)))
+    (setq techela-courses (list (cons tq-course-name (list tq-root-directory tq-course-directory)))))
+  (customize-save-variable 'techela-courses techela-courses)
   
   ;; finally open the syllabus
   (find-file (expand-file-name "syllabus.org" tq-course-directory))
+  (toggle-read-only)
   (techela-mode)
 
   ;; let user know if an update is needed
   (when (> (tq-get-num-incoming-changes) 0)
     (message "%s is out of date. Please update" tq-course-name)
-    (easy-menu-add-item my-menu nil
+    (easy-menu-add-item techela-menu nil
 			["Update Course needed!" tq-update t])))
 
-(defalias 'techela 'tq-syllabus)
 
 (defun tq-in-git-p (&optional debug)
   "Return status for whether `default-directory' is in a git repo.
 Optional argument DEBUG switch to output buffer if the command fails."
   (interactive)
-  (let ((status (= 0 (shell-command "git rev-parse --is-inside-work-tree"))))
-    (when debug
-      (unless status
-	(switch-to-buffer "*Shell Command Output*")))
-    status))
+  (mygit "git rev-parse --is-inside-work-tree"))
 
 
 (defun tq-get-num-incoming-changes ()
   "Return number of changes the remote is different than local."
+  (interactive)
   (unless (tq-in-git-p)
-    (error "You are not in a git repo.  We think you are in %s" default-directory))
-  (shell-command "git fetch origin")
-  (string-to-number (shell-command-to-string "git rev-list HEAD...origin/master --count")))
+    (error "You are not in a git repo.  We think you are in %s." default-directory))
+  (mygit "git fetch origin")
+  (string-to-number (nth 1 (mygit "git rev-list HEAD...origin/master --count"))))
 
 
 (defun tq-clone-repo (repo)
-  "Clone repo if needed. do nothing if directory exists. clone one directory up from the location of course.el.
-
-REPO should not have the extension .git on it"
-  (let ((local-repo (expand-file-name repo tq-root-directory)))
-    (if (file-exists-p local-repo)
-	local-repo ; the repo exists
-      ;; else, we clone it
-      (let* ((cmd (format "git clone %s@%s:%s.git %s" tq-current-course tq-git-server repo local-repo))
-	     (status (= 0 (shell-command cmd))))
-	(message "status = %s" status)
-	(if status
-	    local-repo	  
-	  (switch-to-buffer "*Shell Command Output*")
-	  (error "Problem cloning %s" repo))))))
+  "Clone REPO into current directory if needed. If REPO exists,
+do not do anything. REPO should not have the extension .git on
+it. If you want to clone it somewhere else, temporarily define
+default-directory.
+"
+  (if (file-exists-p repo)
+      repo
+    (mygit (format "git clone %s@%s:%s.git" tq-current-course tq-git-server repo))
+    repo))
 
 
 (defun tq-clone-and-open (repo)
   "Clone REPO and open it."
-  (find-file (tq-clone-repo repo)))
+  (let ((default-directory tq-root-directory))    
+    (tq-clone-repo repo)
+    (find-file (expand-file-name (concat repo ".org") repo))))
 
 
 (defun tq-get-assignment (label)
   "Clone the repo corresponding to LABEL and open the directory."
   (interactive "sLabel: ")
-  (let ((repo (format "%s-%s-%s" tq-current-course *andrewid* label)))
-    (tq-clone-and-open repo)))
+  (let ((default-directory tq-root-directory))
+    (let ((repo (format "%s-%s-%s" tq-current-course *andrewid* label)))
+      ;; clone and open label.org
+      (tq-clone-repo repo)
+      (find-file (expand-file-name (concat label ".org") repo)))))
 
 
 (require 'net-utils)
@@ -188,19 +260,24 @@ REPO should not have the extension .git on it"
   (interactive)
   (save-some-buffers t t)
   (insert-system-info)
-  (if (and
-       (= 0 (shell-command "git add *"))
-       (= 0 (shell-command "git commit -am \"turning in\""))
-       (= 0 (shell-command "git push")))
-      (message "Woohoo! You turned it in!")
-    (switch-to-buffer "*Shell Command Output*")))
+  (mygit "git add *")
 
+  (let ((status (car (mygit "git commit -am \"turning in\""))))
+    (unless (or (= 0 status)  ; no problem
+		(= 1 status)) ; no change in files
+      (switch-to-buffer "*techela-log*")
+      (error "Problem committing. Check the logs")))
+  (mygit "git push")
+  (message "Woohoo! You turned it in!"))
 
 (defun tq-update ()
   "Run git pull.  Refresh file currently visited."
   (interactive)
-  (shell-command "git pull")
-  (revert-buffer t t))
+  (mygit "git pull")
+  (revert-buffer t t)
+  (easy-menu-remove-item techela-menu nil "Update Course needed!")
+  (techela-mode 1))
+
 
 (add-to-list 'org-agenda-custom-commands
       '("c" "Course Agenda"
@@ -248,7 +325,7 @@ repo: %s
 		 (buffer-file-name)
 		 (what-line)
 		 (thing-at-point 'line)
-		 (shell-command-to-string "git config --get remote.origin.url"))))
+		 (mygit "git config --get remote.origin.url" t))))
 
     (compose-mail-other-frame)
     (message-goto-to)
@@ -261,6 +338,31 @@ repo: %s
     (next-line 2)         ; and down two lines
     (message "Type C-c C-c to send message")))
 
+(defun tq-send-error-report ()
+  (interactive)
+  (compose-mail-other-frame)
+   (message-goto-to)
+   (insert "jkitchin@andrew.cmu.edu")
+   (message-goto-subject)
+   (insert (format "[%s] debug report" tq-current-course))
+   (message-goto-body)
+   (insert "Tell match-end what you were doing. Then press C-c C-c to send the message.
+
+
+Messages\n==========\n")
+   (when (get-buffer "*mygit-process*")
+     (insert (with-current-buffer "*mygit-process*" (buffer-string)))
+     (insert "\n"))
+   (when (get-buffer "*techela-log*")
+     (insert (with-current-buffer "*techela-log*" (buffer-string)))
+     (insert "\n"))
+   (message-goto-body) ; go back to beginning of email body
+   
+   (next-line 2)         ; and down two lines
+   
+   (message "Type C-c C-c to send message"))
+
+	   
 ;;;; links
 
 ;; This is a personal assignment
@@ -288,9 +390,12 @@ repo: %s
 	  (data-file (format "%s-%s.dat" *andrewid* label)))
      (with-temp-file data-file
        (insert data))
-     (shell-command (format "git add %s" data-file))
-     (shell-command (format "git commit -m \"%s\"" data-file))
-     (shell-command "git push"))))
+     (mygit (format "git add %s" data-file))
+     (mygit (format "git commit -m \"%s\"" data-file))
+     (mygit "git push"))))
+
+
+
 
 ;;;; menu and minor mode
 
@@ -302,13 +407,14 @@ repo: %s
     map)
   "Keymap for function `techela-mode'.")
 
-(easy-menu-define my-menu techela-mode-map "My own menu"
+(easy-menu-define techela-menu techela-mode-map "My own menu"
   '("techela"
     ["Get assignment " tq-get-assignment t]
     ["Turn assignment in" tq-turn-it-in t]
     ["Course agenda" tq-agenda t]
 ;    ["Get grade report" tq-grade-report t]
     ["Email" tq-email t]
+    ["Send error report" tq-send-error-report t]
 ))
 
 (define-minor-mode techela-mode
