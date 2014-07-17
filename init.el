1112db16f447a9e561e7f1d3cc05d7ade340148f
John Kitchin <jkitchin@andrew.cmu.edu>
2014-07-16 21:57:49 -0400

    add comment to update

--- init.el
+++ init.el
@@ -16,7 +16,8 @@
 (add-to-list 'load-path user-dir)
 
 ;; check status of jmax, and update if needed. We use a timeout of 10
-;; seconds which defaults to no update. 
+;; seconds which defaults to no update. We could use a variable
+;; setting here but where would it get set?
 (shell-command "git fetch")
 (unless (= 0 (string-to-number
 	      (shell-command-to-string
