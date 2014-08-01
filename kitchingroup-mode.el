7cd3a90b6ff7e18897d5e9f365a1b19b5f5beeba
John Kitchin <jkitchin@andrew.cmu.edu>
2014-08-01 12:49:54 -0400

    change menu command

--- kitchingroup-mode.el
+++ kitchingroup-mode.el
@@ -77,7 +77,7 @@
     ("org-mode"
      ["Toggle symbols" org-toggle-pretty-entities t]
      ["Toggle inline images" org-toggle-inline-images t]
-     ["Toggle LaTeX images" org-preview-latex-fragment t]
+     ["Toggle LaTeX images" org-toggle-latex-overlays t]
      ("editing"
       ["Insert citation" org-ref-insert-cite-link t]
       ["Insert ref link" org-ref-insert-ref-link t]
