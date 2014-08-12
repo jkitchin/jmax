081aa26199a0b58c09cf177c0bfae3cbaab95f36
John Kitchin <jkitchin@andrew.cmu.edu>
2014-08-12 19:06:19 -0400

    fix bug in cloning initial course

--- techela/techela.el
+++ techela/techela.el
@@ -73,7 +73,7 @@ The user ssh.pub key must be registered in the course."
     ;; do not clone if the directory exists.
     (unless (and tq-course-directory (file-exists-p tq-course-directory))
       (let ((default-directory (file-name-as-directory tq-root-directory)))
-	(mygit (format "git clone git://%s/course" tq-git-server)))))
+	(mygit (format "git clone git://%s@%s/course" course tq-git-server)))))
   
   ;; finally open the syllabus
   (find-file (expand-file-name "syllabus.org" tq-course-directory))
