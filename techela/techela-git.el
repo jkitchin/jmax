2cdd06ca57635525bdaeed7b4e88cbc65804d1d5
John Kitchin <jkitchin@andrew.cmu.edu>
2014-09-18 14:49:42 -0400

    new functions for updating a file

--- techela/techela-git.el
+++ techela/techela-git.el
@@ -77,4 +77,118 @@ returns \" M filename
 	      (file-name-nondirectory
 	       (buffer-file-name)))))))
 
+
+(defvar ta-git-unmerged-states
+  '(("D" "D")
+    ("A" "U")
+    ("U" "D")
+    ("U" "A")
+    ("D" "U")
+    ("A" "A")
+    ("U" "U"))
+  "List of XY states a file can be in that indicate an unmerged state")
+
+(defun ta-git-make-repo-clean ()
+  "Handle every line in git status --porcelain and leave repo in clean state.
+
+This may happen before or after a merge. Before a merge, we handle each line. After a merge, we add everything at once, and then commit the merge.
+"
+  (interactive)
+
+  (let* ((merge-p nil)
+	 (lines (loop for line in
+		      (split-string
+		       (shell-command-to-string "git status --porcelain") "\n")
+		      when (not (string= "" line))
+		      collect		    
+		      (let* ((X (substring line 0 1))
+			     (Y (substring line 1 2))
+			     (PATHS (split-string (substring line 3)))
+			     (FROM (nth 0 PATHS))
+			     ;; for a rename there will be PATH1 -> PATH2
+			     (TO (if (= 3 (length PATHS)) (nth 2 PATHS) nil)))
+			(when (-contains? ta-git-unmerged-states (list X Y))
+			  (setq merge-p t))
+			(message "%s" (list X Y FROM TO line))
+			(list X Y FROM TO line)))))
+
+    (if merge-p
+	;; we need to add everything, and commit it.
+	(progn
+	  (message "Processing an unmerged state")
+	  (mygit (format "git add *"))
+	  (mygit (format "git commit -am \"committing a merge\"")))
+
+      ;; we are not in a merge, so we just handle each line.  now we
+      ;; loop through the lines and handle them. We try to handle
+      ;; individual lines so we can create more meaningful, single
+      ;; file commits.
+      (loop for (X Y FROM TO LINE) in lines
+	    do
+	    (message "handling %s" LINE)
+	    (cond
+	     ;; untracked files get added and committed.
+	     ((equal (list X Y) '("?" "?"))
+	      (mygit (format "git add %s" FROM))
+	      (mygit
+	       (format "git commit %s -m \"adding untracked file: %s.\""
+		       FROM FROM)))
+	 
+	     ;; user rename
+	     ((equal (list X Y) '("R" " "))
+	      (mygit (format "git commit %s -m \"rename %s to %s\"" FROM FROM TO))
+	      (mygit (format "git add %s" TO))
+	      (mygit (format "git commit %s -m \"adding %s\"" TO TO)))
+	 
+	     ;; rename and modify
+	     ((equal (list X Y) '("R" "M"))
+	      ;; commit the rename
+	      (mygit (format "git commit %s -m \"rename %s to %s\"" FROM FROM TO))
+	      (mygit (format "git commit %s -m \"changes in %s\"" TO TO)))
+	 
+	     ;; added file
+	     ((equal (list X Y) '("A" " "))
+	      (mygit (format "git commit %s -m  \"Adding %s\"" FROM FROM)))
+	 
+	     ;; deleted file
+	     ((equal (list X Y) '(" " "D"))
+	      (mygit (format "git commit %s -m \"Deleting %s\"" FROM FROM)))
+	 
+	     ;; modified file
+	     ((or (string= X "M")
+		  (string= Y "M"))
+	      (mygit (format "git add %s" FROM))
+	      (mygit (format "git commit %s -m \"changes in %s\"" FROM FROM)))
+      
+	     ;; catch everything else
+	     (t
+	      (mygit (format "git add %s" FROM))
+	      (mygit (format "git commit %s -m \"%s\"" FROM LINE))))))))
+
+  
+(defun ta-git-update-file ()
+  "Update the current file.
+
+This is tricky because we cannot just pull a file. We have to save all files, commit them, then pull. It may be the case that this causes a merge conflict, which we then need to address.
+
+The strategy is to check git status --porcelain first, and get the repo into a clean state. Then we do the pull. Then, we check again and get back to a clean state if needed.
+"
+  (interactive)
+  
+  (save-some-buffers t t) ; make sure all files are saved.
+  ;; first clean the repo
+  (ta-git-make-repo-clean)
+
+  ;; Next we are going to fetch
+  (mygit "git fetch")
+
+  ;; then merge, we assume with origin/master
+  (mygit "git merge origin/master -m \"merging origin/master in\"")
+
+  ;; there may be merge conflicts. we take them, and make the repo
+  ;; clean again.
+  (ta-git-make-repo-clean)
+  (revert-buffer t t) ;; update this buffer
+  )
+
 (provide 'techela-git)
