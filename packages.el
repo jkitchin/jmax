4350967872423f9145877580abb9db472f6b87ec
John Kitchin <jkitchin@andrew.cmu.edu>
2015-01-07 12:52:31 -0500

    add helm

--- packages.el
+++ packages.el
@@ -7,7 +7,7 @@
 (setq package-archives
       '(("org"         . "http://orgmode.org/elpa/")
 	("gnu"         . "http://elpa.gnu.org/packages/")
-;	("original"    . "http://tromey.com/elpa/")	
+;	("original"    . "http://tromey.com/elpa/")
 	("melpa" . "http://melpa.org/packages/" )
 ;	("marmalade"   . "http://marmalade-repo.org/packages/")
 	))
@@ -19,7 +19,7 @@
 (defvar starter-kit-packages
   (list 'flx-ido 'ido-ubiquitous 'smex
         'yasnippet
-        'magit 
+        'magit
 	'bbdb 'bbdb-ext
         'auctex 'reftex
         'undo-tree
@@ -29,6 +29,7 @@
 	'org-plus-contrib
 	'elpy
 	'jedi 'jedi-direx
+	'helm
 	'f ; file functions https://github.com/rejeep/f.el
 	's ; string functions
 	'dash ; list functions
