c16973f79d9d5109c2a317977b64694d0b1bd276
John Kitchin <jkitchin@andrew.cmu.edu>
2015-01-11 18:50:13 -0500

    fix paren error

--- techela/techela.el
+++ techela/techela.el
@@ -144,9 +144,9 @@ The user ssh.pub key must be registered in the course."
     ;; do not clone if the directory exists.
     (unless (and tq-course-directory (file-exists-p tq-course-directory))
       (let ((default-directory (file-name-as-directory tq-root-directory)))
-	(when (not = 0 (car (mygit (format "git clone %s@%s:course"
+	(when (not (= 0 (car (mygit (format "git clone %s@%s:course"
 					   tq-current-course
-					   tq-git-server))))
+					   tq-git-server)))))
 	  (error "Could not clone course. Try again later.")))))
 
   ;; let user know if an update is needed
