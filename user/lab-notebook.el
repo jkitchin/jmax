60e40bcb0e01f1db1ef95ba575049260df39a1fd
John Kitchin <jkitchin@andrew.cmu.edu>
2015-03-09 21:09:18 -0400

    removing lentic code. Trying out outshine.

--- user/lab-notebook.el
+++ user/lab-notebook.el
@@ -3,33 +3,24 @@
 
 ;;; Commentary:
 
-;; ;;; Reminders about lentic:
-;; Lentic key binding reminders
-;; C-c , h  Move here in other view
-;; C-c , s  Swap windows
-;; [[elisp:lentic-mode-split-window-right]]  C-c o to open this link and make the view
-
 ;;; Code:
-;; ** Setup
-;; #+BEGIN_SRC emacs-lisp
+;; * Setup
+
 (defvar nb-notebook-file
   "~/Dropbox/org-mode/lab-notebook.org"
   "Notebook file where entries are stored.")
-;; #+END_SRC
-;; ** A capture template
-;; #+BEGIN_SRC emacs-lisp
+
+;; * A capture template
 ;; capture to a notebook
 (add-to-list
  'org-capture-templates
  '("n" "Notebook" entry (file+datetree nb-notebook-file "Notebook")
   "* %?\nEntered on %U\n  %i\n"))
 
-;; #+END_SRC
 
-;; ** Integration with git
-;; In the interest of streamlining interactions with version control, here we anticipate our needs and make a function to "do what I need" next in vc. On an empty directory, make it under version control, on a new file, make it under version control. otherwise, commit the changes.
 
-;; #+BEGIN_SRC emacs-lisp
+;; * Integration with git
+;; In the interest of streamlining interactions with version control, here we anticipate our needs and make a function to "do what I need" next in vc. On an empty directory, make it under version control, on a new file, make it under version control. otherwise, commit the changes
 (require 'vc)
 (defun dwin-vc ()
   "Do what I need in vc next.
@@ -68,26 +59,21 @@ Add current file if not in vc, then prompt for commit message"
 	  (message "state of %s = %s. No action coded." (buffer-file-name) state)))))
      ;; catch case not in vc
      (t
-      (message "Not in a VC repo. Perhaps run vc-create-repo?"))
-     )))
-;; #+END_SRC
+      (message "Not in a VC repo. Perhaps run vc-create-repo?")))))
 
 
-;; ** lab-notebook
-;; We want a nice function to open our lab notebook, with helm buffer to select headlines.
-;; #+BEGIN_SRC emacs-lisp
+
+;; * lab-notebook
+;; We want a nice function to open our lab notebook, with helm buffer to select headlines
 (defun lab-notebook ()
+  "Open the `lab-notebook'."
   (interactive)
   (find-file nb-notebook-file)
   (helm-org-in-buffer-headings)
   (lab-notebook-mode))
-;; #+END_SRC
 
 
-
-
-;; ** A minor mode
-;; #+BEGIN_SRC emacs-lisp
+;; * A minor mode
 ;;;###autoload
 (define-minor-mode lab-notebook-mode
   "Toggle lab notebook mode.
@@ -99,23 +85,13 @@ This will prompt you to commit a file when you kill a buffer
   :lighter " LN"
   ;; keybindings
   :keymap (let ((map (make-sparse-keymap)))
-            ;(define-key map (kbd "<f5>") 'export-bibtex)
             map)
   ;; body
   (add-hook 'kill-buffer-hook 'dwin-vc nil 'make-it-local))
 
 
-;; #+END_SRC
-
-
-
-
 ;; * The end
-;; #+BEGIN_SRC emacs-lisp
-(provide 'lab-notebook)
-;; #+END_SRC
-;; ;;; lab-notebook.el ends here
 
-;; # Local Variables:
-;; # lentic-init: lentic-orgel-org-init
-;; # End:
+(provide 'lab-notebook)
+
+;;; lab-notebook.el ends here
