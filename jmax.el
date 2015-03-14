09385c8a8c811771420d13529469e97b44afa5c0
John Kitchin <jkitchin@andrew.cmu.edu>
2015-03-14 09:24:41 -0400

    added header to jmax for testing

--- jmax.el
+++ jmax.el
@@ -1,3 +1,33 @@
+;;; jmax.el --- Customization for jmax        -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  John Kitchin
+
+;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
+;; URL: https://github.com/jkitchin/jmax
+;; Version: 0.1
+;; Keywords: org-mode
+;; Package-Requires: ()
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 ;; * Packages
 (defcustom jmax-user-theme 'leuven
   "User defined theme to load")
@@ -313,3 +343,4 @@
 (load-theme jmax-user-theme t)
 
 (provide 'jmax)
+;;; jmax.el ends here
