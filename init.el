6e18b149f78d304f9d246ea7ce88b67d498f36ab
John Kitchin <jkitchin@andrew.cmu.edu>
2015-03-18 09:14:35 -0400

    make auto-update optional, but on by default.

--- init.el
+++ init.el
@@ -3,9 +3,8 @@
 
 ;;; Commentary:
 ;;
-;; Part of the Emacs Starter Kit
-;;
-;; This is the first thing to get loaded.
+;; This is a starter kit for jmax. This package provides a customized setup for
+;; emacs that we use daily for scientific programming and publication.
 ;;
 
 ;;; Code:
@@ -23,25 +22,38 @@
 (add-to-list 'load-path starter-kit-dir)
 (add-to-list 'load-path user-dir)
 
+(defvar jmax-auto-update t
+  "Determines if jmax will automatically update itself.")
+
+;; we load the user/preload.el file if it exists. This lets users define
+;; variables that might affect packages when they are loaded, e.g. key-bindings,
+;; etc... In particular, this is needed for setting some key-bindings in
+;; jmax-bibtex. Also, you can turn off jmax-auto-update here.
+
+(let ((preload (expand-file-name "user/preload.el" starter-kit-dir)))
+  (when (file-exists-p preload)
+    (load preload)))
+
 ;; check status of jmax, and update if needed.  This is not super
 ;; robust if the user has changed jmax, since we do not check if your
 ;; repo is clean.
-(let ((default-directory starter-kit-dir))
-  (shell-command "git fetch")
-  (shell-command "git submodule init")
-  (shell-command "git submodule update")
-  (let ((output (shell-command-to-string
-		 "git rev-list --count --left-right HEAD...origin/master"))
-	(local-changes) (remote-changes))
-    (setq local-changes (string-to-number (nth 0 (split-string output))))
-    (setq remote-changes (string-to-number (nth 1 (split-string output))))
-    (when (> remote-changes 0)
-      (when (let ((last-nonmenu-event nil))
-	      (y-or-n-p "jmax is not up to date. Update now?"))
-	(message "updating jmax now")
-	(shell-command "git pull")
-	(shell-command "git submodule init")
-	(shell-command "git submodule update")))))
+(if jmax-auto-update
+    (let ((default-directory starter-kit-dir))
+      (shell-command "git fetch")
+      (shell-command "git submodule init")
+      (shell-command "git submodule update")
+      (let ((output (shell-command-to-string
+		     "git rev-list --count --left-right HEAD...origin/master"))
+	    (local-changes) (remote-changes))
+	(setq local-changes (string-to-number (nth 0 (split-string output))))
+	(setq remote-changes (string-to-number (nth 1 (split-string output))))
+	(when (> remote-changes 0)
+	  (when (let ((last-nonmenu-event nil))
+		      (y-or-n-p "jmax is not up to date. Update now?"))
+		(message "updating jmax now")
+		(shell-command "git pull")
+		(shell-command "git submodule init")
+		(shell-command "git submodule update"))))))
 
 (require 'packages)
 (require 'jmax)
