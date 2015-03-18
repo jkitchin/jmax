cb5f2b24c4c3255688ed8b876c0ac1fa2b863cb3
John Kitchin <jkitchin@andrew.cmu.edu>
2015-03-18 13:53:53 -0400

    add python session cleanup code

--- jmax-org.el
+++ jmax-org.el
@@ -460,6 +460,32 @@ FEATURE is a symbol, and it is loaded from an org-file by the name of FEATURE.or
 
 
 
+;; * Python sessions
+(defun org-babel-python-strip-session-chars ()
+  "Remove >>> and ... from a Python session output."
+  (when (and (string=
+	      "python"
+	      (org-element-property :language (org-element-at-point)))
+	     (string-match
+	      ":session"
+	      (org-element-property :parameters (org-element-at-point))))
+
+    (save-excursion
+      (when (org-babel-where-is-src-block-result)
+	(goto-char (org-babel-where-is-src-block-result))
+	(end-of-line 1)
+	;(while (looking-at "[\n\r\t\f ]") (forward-char 1))
+	(while (re-search-forward
+		"\\(>>> \\|\\.\\.\\. \\|: $\\|: >>>$\\)"
+		(org-element-property :end (org-element-at-point))
+		t)
+	  (replace-match "")
+	  ;; this enables us to get rid of blank lines and blank : >>>
+	  (beginning-of-line)
+	  (when (looking-at "^$")
+	    (kill-line)))))))
+
+(add-hook 'org-babel-after-execute-hook 'org-babel-python-strip-session-chars)
 ;; * Miscellaneous
 (defun sa-ignore-headline (contents backend info)
   "Ignore headlines with tag `ignoreheading'.
