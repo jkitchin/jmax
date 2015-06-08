d7f461091f33b603aaecb6e49b78bab46e5ceadb
Jacob Boes <jboes@andrew.cmu.edu>
2015-06-08 12:59:29 -0400

    update to jmax for windows users

--- emacs-24.3/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/d/usr/lib"
-XML2_LIBS="-lxml2 -lz   -liconv  "
-XML2_INCLUDEDIR="-I/d/usr/include/libxml2"
-MODULE_VERSION="xml2-2.7.8"
-
