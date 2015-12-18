1e76ce7b19ccaf9a8b94ffdec7624f01d7e14098
John Kitchin <jkitchin@andrew.cmu.edu>
2015-12-17 21:36:39 -0500

    undo the tab function.
    it doesn't cycle on src blocks yet.

--- jmax-org.el
+++ jmax-org.el
@@ -180,6 +180,9 @@
 (add-to-list 'org-structure-template-alist
              '("sh" "#+BEGIN_SRC sh\n?\n#+END_SRC" "<src lang=\"shell\">\n?\n</src>"))
 
+(add-to-list 'org-structure-template-alist
+             '("lh" "#+latex_header: " ""))
+
 ;;* Babel settings
 ;; do not evaluate code on export by default
 (setq org-export-babel-evaluate nil)
@@ -462,24 +465,24 @@ citecolor=blue,filecolor=blue,menucolor=blue,urlcolor=blue"
 
 
 ;;* Python sessions
-(defun org-mode-tab (&optional arg)
-  "In org-mode make tab cycle on headlines, or insert 4 spaces."
-  (interactive "P")
-  (cond
-   ;; Cycle headlines
-   ((org-on-heading-p)
-    (org-cycle arg))
-   ;; expand source blocks
-   ((looking-back (format
-		   "^<%s"
-		   (regexp-opt (mapcar 'car org-structure-template-alist)))
-		  (line-beginning-position))
-    (org-try-structure-completion))
-   ;; or insert 4 spaces
-   (t
-    (insert "    "))))
+;; (defun org-mode-tab (&optional arg)
+;;   "In org-mode make tab cycle on headlines, or insert 4 spaces."
+;;   (interactive "P")
+;;   (cond
+;;    ;; Cycle headlines
+;;    ((org-on-heading-p)
+;;     (org-cycle arg))
+;;    ;; expand source blocks
+;;    ((looking-back (format
+;;		   "^<%s"
+;;		   (regexp-opt (mapcar 'car org-structure-template-alist)))
+;;		  (line-beginning-position))
+;;     (org-try-structure-completion))
+;;    ;; or insert 4 spaces
+;;    (t
+;;     (insert "    "))))
 
-(define-key org-mode-map (kbd "<tab>") 'org-mode-tab)
+;; (define-key org-mode-map (kbd "<tab>") 'org-mode-tab)
 
 (defun org-babel-python-strip-session-chars ()
   "Remove >>> and ... from a Python session output."
