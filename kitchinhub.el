ec1c73c2695db17fcaaf935b25f0264d32cc0f1c
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-07 11:11:50 -0500

    add a bunch of other repos

--- kitchinhub.el
+++ kitchinhub.el
@@ -8,13 +8,25 @@
 
 ;;; Code:
 
-(defvar kitchinhub-root "~/Dropbox/KitchinHUB/"
-  "Root directory to clone repos in.")
-
 (defvar kitchinhub-userpass nil
   "User:password. Set this in a private place!")
 
-(defun kitchinhub-repos ()
+(defvar kitchinhub-root "~/Dropbox/KitchinHUB/"
+  "Root directory to clone KitchinHUB repos in.")
+
+(defvar jkitchin-github-root "~/Dropbox/jkitchin-github/"
+  "Root directory to clone jkitchin repos in.")
+
+(defvar github-root "~/Dropbox/github/"
+  "Root directory to clone other github repos in.")
+
+(loop for dir in (list kitchinhub-root jkitchin-github-root github-root)
+      do
+      (unless (file-directory-p dir)
+	(make-directory dir t)))
+
+
+(defun kitchinhub-repo-candidates ()
   "Get list of helm candidates of KitchinHUB repos."
   (let ((json (json-read-from-string
 	       (shell-command-to-string
@@ -28,21 +40,114 @@
 			(cdr (assoc 'name repo))))))
 
 
-(defun kitchinhub (repo)
-  "Select a KitchinHUB REPO and open it.
-Selection is done with helm."
-  (interactive (list (helm :sources `((name . "KitchinHUB Repos")
-				      (candidates . ,(kitchinhub-repos))
-				      (action . identity)))))
+(defun jkitchin-repo-candidates ()
+  "Get a list of helm candidates of repos from
+http://github.com/jkitchin."
+  (let ((json (json-read-from-string
+	       (shell-command-to-string
+		"curl -s https://api.github.com/users/jkitchin/repos"))))
+    (loop for repo across json
+	  collect (cons (format "%30s | %s"
+				(cdr (assoc 'name repo))
+				(cdr (assoc 'description repo)))
+			(cdr (assoc 'name repo))))))
 
-  ;; make sure we have it
+
+(defun kitchinhub-open-repo (repo)
+  "Open REPO from KitchinHUB."
   (unless (file-directory-p (expand-file-name repo kitchinhub-root))
     (let ((default-directory (file-name-as-directory kitchinhub-root)))
-      (shell-command (format  "git clone git@github.com:KitchinHUB/%s.git" repo))))
+      (shell-command
+       (format
+	"git clone git@github.com:KitchinHUB/%s.git" repo))))
 
   ;; Now open it
   (find-file (expand-file-name repo kitchinhub-root))
   (magit-status))
+
+
+(defun jkitchin-open-repo (repo)
+  "Open REPO from jkitchin@github."
+  (unless (file-directory-p (expand-file-name repo jkitchin-github-root))
+    (let ((default-directory (file-name-as-directory jkitchin-github-root)))
+      (shell-command
+       (format  "git clone git@github.com:jkitchin/%s.git" repo))))
+
+  ;; Now open it
+  (find-file (expand-file-name repo jkitchin-github-root))
+  (magit-status))
+
+
+(defun github-open-repo (repo)
+  "Open REPO from github."
+  ;; assume repo is in form username/repo-dir
+  (let ((repo-dir (file-name-nondirectory repo)))
+    (unless (file-directory-p (expand-file-name repo-dir github-root))
+      (let ((default-directory (file-name-as-directory github-root)))
+	(shell-command (format  "git clone git@github.com:%s.git" repo))))
+
+    ;; Now open it
+    (find-file (expand-file-name repo-dir github-root))
+    (magit-status)))
+
+
+(defun kitchinhub (repo)
+  "Select a KitchinHUB REPO and open it.
+Selection is done with helm."
+  (interactive (list (helm :sources `(((name . "KitchinHUB Repos")
+				       (candidates . ,(kitchinhub-repo-candidates))
+				       (action . kitchinhub-open-repo))
+				      ((name . "jkitchin@github")
+				       (candidates . ,(jkitchin-repo-candidates))
+				       (action . jkitchin-open-repo)))))))
+
+;; * Different approach with asynchronous sources (I think)
+
+(defvar kitchinhub-source (helm-build-async-source
+			      "KitchinHUB Repos async"
+			    :candidates-process 'kitchinhub-repo-candidates
+			    :action '(("Open" . kitchinhub-open-repo))))
+
+
+(defvar jkitchin-github-source (helm-build-async-source
+				   "jkitchin@github async"
+			    :candidates-process 'jkitchin-repo-candidates
+			    :action '(("Open" . jkitchin-open-repo))))
+
+
+(defun github-cloned-repos ()
+  "List installed repos in `github-root'."
+  (f-entries github-root))
+
+
+(defvar github-source (helm-build-sync-source
+			  "GitHUB cloned repos"
+			:candidates 'github-cloned-repos
+			:action 'find-file))
+
+(defvar github-url-source (helm-build-sync-source
+			      "GitHUB urls"
+			    :candidates '("https://github.com/"
+					  "https://github.com/jkitchin/"
+					  "https://github.com/KitchinHUB")
+			    :action 'browse-url))
+
+
+;; I don't know how to use the helm-build-sync-source command to get a fallback.
+(defvar github-fallback-source '((name . "Clone github repo")
+				(dummy)
+				(action . github-open-repo)))
+
+
+(defun github ()
+  "Run Helm to select a git repo from jkitchin or KitchinHUB."
+  (interactive)
+  (helm :sources '(jkitchin-github-source
+		   kitchinhub-source
+		   github-source
+		   github-url-source
+		   github-fallback-source)))
+
 
 (provide 'kitchinhub)
 
