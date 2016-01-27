38a1c1a145fcd9048d9fc5bf573f4cb266c99afd
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-27 16:15:35 -0500

    add creation of executable script.

--- install-jmax-mac.sh
+++ install-jmax-mac.sh
@@ -18,4 +18,12 @@ if [ ! -d "jmax" ]; then
 fi
 
 echo "jmax is installed. To use it, run this command in your terminal."
-echo "emacs -q -l `pwd`/init.el"
+echo "emacs -q -l `pwd`/jmax/init.el"
+echo "or"
+echo "run the jmax.sh script created in this directory as ./jmax.sh in the terminal."
+
+echo "#!/bin/bash
+emacs -q -l `pwd`/jmax/init.el
+#end" > jmax.sh
+chmod +x jmax.sh
+#end
