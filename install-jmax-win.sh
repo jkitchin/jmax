65daf600697b3aa87bb4a9b9e5cdfef80cfa9b03
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-27 16:45:08 -0500

    windows install script

---
+++ install-jmax-win.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Now clone jmax
+if [ ! -d "jmax" ]; then
+  git clone https://github.com/jkitchin/jmax.git
+fi
+
+echo "jmax is installed. To use it, run this command in your terminal."
+echo "emacs -q -l `pwd`/jmax/init.el"
+echo "or"
+echo "run the jmax.sh script created in this directory as ./jmax.sh in the terminal."
+
+echo "#!/bin/bash
+emacs -q -l `pwd`/jmax/init.el
+#end" > jmax.sh
+chmod +x jmax.sh
+#end
