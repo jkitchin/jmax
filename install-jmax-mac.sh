6e2272596e672baf6577bdb8579af2d5e6b864cd
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-27 16:05:37 -0500

    install-script for mac

---
+++ install-jmax-mac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check for brew
+command -v brew >/dev/null 2>&1 || { echo >&2 "I require brew but it's not installed.  Installing it now.";
+				     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+
+# Check for git
+command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Installing it now.";
+				    brew install git; }
+
+# Check for emacs
+command -v emacs >/dev/null 2>&1 || { echo >&2 "I require emacs but it's not installed.  Installing it now.";
+				    brew install emacs --with-gnutls --with-imagemagick --with-librsvg --with-x11 --use-git-head --HEAD --with-cocoa; }
+
+# Now clone jmax
+if [ ! -d "jmax" ]; then
+  git clone https://github.com/jkitchin/jmax.git
+fi
+
+echo "jmax is installed. To use it, run this command in your terminal."
+echo "emacs -q -l `pwd`/init.el"
