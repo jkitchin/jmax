27404a3b5ab099803bce5874a12b695e58cf10ca
John Kitchin <jkitchin@andrew.cmu.edu>
2016-03-07 16:35:08 -0500

    add header

--- vipyr-hydra.el
+++ vipyr-hydra.el
@@ -1,3 +1,28 @@
+;;; vipyr-hydra.el --- vipyr-hydra for Python navigation/editing  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  John Kitchin
+
+;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
 
 (require 'hydra)
 
@@ -360,9 +385,9 @@ _r_: to repl
 	     (set-mark (point))
 	     (python-nav-end-of-statement)
 	     (elpy-shell-send-region-or-buffer)
-	     (deactivate-mark))))))
-
-
-  )
+	     (deactivate-mark)))))))
 
 (global-set-key (kbd "s-v") 'vipyr/body)
+
+(provide 'vipyr-hydra)
+;;; vipyr-hydra.el ends here
