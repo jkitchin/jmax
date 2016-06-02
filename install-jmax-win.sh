d7012aee7c4b09cd9b6d08f69253507718b52c20
John Kitchin <jkitchin@andrew.cmu.edu>
2016-06-02 13:43:15 -0400

    update windows installer

--- install-jmax-win.sh
+++ install-jmax-win.sh
@@ -5,13 +5,17 @@ if [ ! -d "jmax" ]; then
   git clone https://github.com/jkitchin/jmax.git
 fi
 
+cd jmax
+git submodule https://github.com/jkitchin/emacs-win.git
+git submodule update
+
 echo "jmax is installed. To use it, run this command in your terminal."
-echo "emacs -q -l `pwd`/jmax/init.el"
+echo "`pwd`/jmax/emacs-win/bin/runemacs.exe -q -l `pwd`/jmax/init.el"
 echo "or"
 echo "run the jmax.sh script created in this directory as ./jmax.sh in the terminal."
 
 echo "#!/bin/bash
-`pwd`/jmax/emacs/bin/runemacs.exe -q -l `pwd`/jmax/init.el
+`pwd`/jmax/emacs-win/bin/runemacs.exe -q -l `pwd`/jmax/init.el
 #end" > jmax.sh
 chmod +x jmax.sh
 #end
